c9cfcfc5cb6140bc8c34ada15c27e0710cf7d62b
Arlinnn25 <bintangarlin2020@gmail.com>
2026-09-03 17:32:18 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,17 @@
+public class Biodata {
+    public static void main(String[] args) {
+
+        // Menggunakan println()
+        System.out.println("=== BIODATA DIRI ===");
+        System.out.println("Nama      : Arlin");
+        System.out.println("Prodi   : Sistem Informasi");
+
+        // Menggunakan print()
+        System.out.print("Universitas : ");
+        System.out.print("Universitas Sulawesi Barat\n");
+
+        // Menggunakan printf()
+        System.out.printf("Umur      : %d tahun%n", 19);
+        System.out.printf("Tinggi    : %.0f cm%n", 158.0);
+    }
+}
