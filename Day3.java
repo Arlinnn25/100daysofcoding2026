ce51736b97b00207113b9ed7e80a96c5a1ce062c
Arlinnn25 <bintangarlin2020@gmail.com>
2026-09-04 10:01:17 +0800

    Add EscapeSequence class demonstrating escape sequences

---
+++ Day3.java
@@ -0,0 +1,10 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+        
+        System.out.println("Baris 1\nBaris 2");           // \n = Membuat baris baru (pindah ke bawah)
+        System.out.println("Nama:\tArlin");             // \t = Memberikan jarak renggang (tab horizontal)
+        System.out.println("Dia berkata: \"Haii\"");     // \" = Menampilkan tanda kutip dua di dalam teks
+        System.out.println("Karakter: \'A\'");           // \' = Menampilkan tanda kutip satu
+        System.out.println("Folder: C:\\Data");          // \\ = Menampilkan satu tanda backslash (\)
+    }
+}
