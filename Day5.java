5a900c5ec066a92e5c77b8dd9ebdaab171e090ec
Arlinnn25 <bintangarlin2020@gmail.com>
2026-09-06 10:45:46 +0800

    Add TipeDataPecahan class with float and double examples

---
+++ Day5.java
@@ -0,0 +1,14 @@
+public class TipeDataPecahan {
+    public static void main(String[] args) {
+        // float wajib diakhiri huruf 'f'
+        float nilaiFloat = 36.8f; 
+        
+        // double tidak perlu akhiran (tipe desimal default)
+        double nilaiDouble = 22.74; 
+
+        // Menampilkan nilai variabel
+        System.out.println("Tipe float  : " + nilaiFloat);
+        System.out.println("Tipe double : " + nilaiDouble);
+
+    }
+}
