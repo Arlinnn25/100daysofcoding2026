27b5dc6833880b2132e4ba1a55c71e4626dc7f51
Arlinnn25 <bintangarlin2020@gmail.com>
2026-09-08 18:36:08 +0800

    Add Day7.java for string data type demonstration

---
+++ Day7.java
@@ -0,0 +1,18 @@
+public class TipeDataStringUntukTeks {
+    public static void main(String[] args) {
+        
+        // 1. Deklarasi dan Inisialisasi String
+        String namaLengkap = "Arlin";
+        String domisili = "Majene, Indonesia";
+        String pesan = "Selamat belajar coding 100day! 🚀";
+
+        // 2. Menampilkan isi String ke layar
+        System.out.println("Nama     : " + namaLengkap);
+        System.out.println("Tinggal  : " + domisili);
+        System.out.println("Pesan    : " + pesan);
+        
+        // 3. Menggabungkan dua String (Concatenation)
+        String sapaan = "Halo, " + namaLengkap + ". Selamat datang!";
+        System.out.println(sapaan);
+    }
+}
