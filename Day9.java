d4c253c5434fc3f6ba07d43dac7174544307aff1
Arlinnn25 <bintangarlin2020@gmail.com>
2026-09-10 18:14:07 +0800

    Add FinalVariabel class to demonstrate final variables

---
+++ Day9.java
@@ -0,0 +1,16 @@
+public class FinalVariabel {
+    public static void main(String[] args) {
+        
+        // final digunakan untuk nilai tetap dan tidak bisa diubah lagi
+        final double Berat_Badan = 49.6;
+        
+        // nilai Berat_Badan tidak bisa diubah lagi
+        System.out.println("Berat Badan :" + Berat_Badan);
+        
+        // contoh constanta lainnya adalah
+        final int Usia = 19;
+        
+        // nilai dari usia tidak bisa diubah lagi
+        System.out.println("Umur :" + Usia);
+    }
+}
