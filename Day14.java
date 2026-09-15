347582139c8a0256fb3b6af6ac1ced4320d19fb3
Arlinnn25 <bintangarlin2020@gmail.com>
2026-09-15 11:28:28 +0800

    Add basic addition and subtraction program

---
+++ Day14.java
@@ -0,0 +1,13 @@
+public class PenjumlahanPengurangan {
+    public static void main (String[] args) {
+        //Mendeklarasikan variabel tipe integer
+        int a = 17;
+        int b = 9;
+        //Proses penjumlahan dan pengurangan
+        int hasilPenjumlahan = a + b;
+        int hasilPengurangan = a - b;
+        //Menampilkan hasil penjumlahan dan pengurungan
+        System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+    }
+}
