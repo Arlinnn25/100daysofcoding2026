8f6c10cb1d464c9659af24cfc9ff374fd8bf20ab
Arlinnn25 <bintangarlin2020@gmail.com>
2026-09-16 21:32:40 +0800

    Add multiplication and division example in Day15.java

---
+++ Day15.java
@@ -0,0 +1,13 @@
+public class PerkaliandanPembagian {
+    public static void main(String[] args) {
+        // Mendeklarasikan variabel tipe integer
+        int a = 15;
+        int b = 5;
+        // Proses perkalian dan pembagian
+        int hasilPerkalian = a * b;
+        int hasilPembagian = a / b;
+        // Menampilkan hasil perkalian dan pembagian
+        System.out.println("Hasil Perkalian : " + hasilPerkalian);
+        System.out.println("Hasil Pembagian : " + hasilPembagian);
+    }
+}
