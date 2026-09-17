e67f3828a7070d184da9a403a706fc90f57aba0d
Arlinnn25 <bintangarlin2020@gmail.com>
2026-09-17 19:35:59 +0800

    Add ModulusAtauSisaBagi class for modulus operation

---
+++ Day16.java
@@ -0,0 +1,11 @@
+public class ModulusAtauSisaBagi {
+    public static void main(String[] args) {
+        //Mendeklarasikan variabel tipe Integer
+        int a = 10;
+        int b = 3;
+        //proses operator aritmatika modulus
+        int hasil = a % b;
+        //Menampilkan hasil modulus dari variabel a dan b
+        System.out.println("Sisa bagi dari 10 % 3 adalah: " + hasil);
+    }
+}
